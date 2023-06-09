6e3c6367160eb5266ac53c6a05d83803b8eb267e
Aleksa Zdravković <aleksazdravkovic@proton.me>
2023-06-09 08:41:09 +0200

    Formatted code

--- config/helpers.js
+++ config/helpers.js
@@ -4,4 +4,4 @@ export const isOneDayApart = (dateString1, dateString2) => {
   const diffInMs = Math.abs(date2.getTime() - date1.getTime());
   const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
   return diffInDays === 1;
-}
+};
