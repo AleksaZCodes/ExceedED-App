2ae1784a66ff6939f3e3c57136fe4ae1c549b763
Aleksa Zdravković <aleksazdravkovic@proton.me>
2023-06-09 09:07:52 +0200

    Added createTextStyle

--- config/helpers.js
+++ config/helpers.js
@@ -1,3 +1,4 @@
+// Checks if the dates are exactly 1 day apart
 export const isOneDayApart = (dateString1, dateString2) => {
   const date1 = new Date(dateString1);
   const date2 = new Date(dateString2);
@@ -5,3 +6,10 @@ export const isOneDayApart = (dateString1, dateString2) => {
   const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
   return diffInDays === 1;
 };
+
+// Used for condensing the code in STYLES
+export const createTextStyle = (size) => ({
+  fontFamily: FONTS.semiBold,
+  color: COLORS.text,
+  fontSize: size,
+});
