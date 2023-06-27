b916e7228eb05c1a0cf29e2867d996d75e6cdafa
Aleksa Zdravković <aleksazdravkovic@proton.me>
2023-06-27 10:05:22 +0200

    Added supabase authentication

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,14 +1,15 @@
 import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import * as Linking from "expo-linking";
 import { useTheme } from "@react-navigation/native";
-import Input from "../components/Input";
 import { Formik } from "formik";
 import { AUTH_SCHEMA, SPACING } from "../config/constants";
 import Button from "../components/Button";
 import InputField from "../components/InputField";
-import { setGlobalState } from "../config/globalState";
 import { useState } from "react";
 import Loading from "../components/Loading";
 import { supabase } from "../config/supabase";
+import * as WebBrowser from "expo-web-browser";
+import TouchableText from "../components/TouchableText";
 
 const SignupScreen = ({ navigation }) => {
   const COLORS = useTheme().colors;
@@ -24,12 +25,19 @@ const SignupScreen = ({ navigation }) => {
       width: "90%",
       justifyContent: "center",
     },
+    text: {
+      marginTop: SPACING.normal,
+      marginBottom: SPACING.normal,
+    },
+    button: {
+      marginBottom: SPACING.normal * 2,
+    },
   });
 
   // Local state
   const [loading, setLoading] = useState(false);
 
-  // Signs the user up with supabase
+  // Signs the user up with email and password
   const signUpWithEmail = async ({ email, password }) => {
     setLoading(true);
 
@@ -40,9 +48,46 @@ const SignupScreen = ({ navigation }) => {
     });
 
     // Display errors if any
-    if (authentication.error) alert(authentication.error.name);
+    if (authentication.error) alert(authentication.error.message);
 
-    console.log(authentication.id);
+    console.log(authentication.data.user.id);
+
+    setLoading(false);
+  };
+
+  // Signs the user up with Google OAuth
+  const signInWithGoogle = async () => {
+    setLoading(true);
+
+    // Fetch the authentication from supabase
+    const authentication = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    const redirectUrl = Linking.makeUrl("auth-callback");
+    console.log(redirectUrl);
+
+    const result = await WebBrowser.openAuthSessionAsync(
+      authentication.data.url,
+      redirectUrl
+    );
+
+    if (result.type === "success" && !result.errorCode) {
+      // Retrieve the access token from the authentication result
+      const access_token = result.url.split("access_token=")[1].split("&")[0];
+
+      // Use the access token to sign in with Supabase
+      const { user, error } = await supabase.auth.signIn({
+        provider: "google",
+        access_token,
+      });
+
+      if (error) {
+        console.error("Error signing in with Google:", error);
+      } else {
+        console.log("Successfully signed in with Google:", user);
+      }
+    }
 
     setLoading(false);
   };
@@ -92,6 +137,19 @@ const SignupScreen = ({ navigation }) => {
               label="NASTAVI"
               onPress={() => formikProps.handleSubmit()}
             />
+
+            <Text style={[STYLES.headlineSmall, styles.text]}>ili</Text>
+
+            <Button
+              type="outline"
+              iconType="ANT"
+              iconName="google"
+              iconColor={COLORS.primary}
+              style={styles.button}
+              label="KORISTEĆI GOOGLE"
+            />
+
+            <TouchableText label="NASTAVI ANONIMNO" />
           </KeyboardAvoidingView>
         )}
       </Formik>
